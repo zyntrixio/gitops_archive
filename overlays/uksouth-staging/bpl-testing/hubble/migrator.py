bdd320afb27980313921865180fb93710f7810bb
Thenuja Viknarajah <thenuja_1@outlook.com>
2023-04-06 16:03:28 +0100

    fix migrator.py in bpl-testing/hubble

--- overlays/uksouth-staging/bpl-testing/hubble/migrator.py
+++ overlays/uksouth-staging/bpl-testing/hubble/migrator.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from alembic import command
 from alembic.config import Config
 from sqlalchemy import text, create_engine
@@ -9,27 +8,18 @@ DB_NAME = "hubble"
 TEMPLATE_DB = f"{DB_NAME}_template"
 ALEMBIC_DIR = "/app/alembic"
 
-postgres = create_engine(f"{DB_BASE}/postgres")
+postgres = create_engine(f"{DB_BASE}/postgres", isolation_level="AUTOCOMMIT")
 with postgres.connect() as connection:
-    connection.execution_options(isolation_level="AUTOCOMMIT").execute(
-        text(f"DROP DATABASE IF EXISTS {TEMPLATE_DB} WITH (FORCE)")
-    )
-    connection.commit()
-    connection.execution_options(isolation_level="AUTOCOMMIT").execute(
-        text(f"DROP DATABASE IF EXISTS {DB_NAME} WITH (FORCE)")
-    )
-    connection.commit()
-    connection.execution_options(isolation_level="AUTOCOMMIT").execute(text(f"CREATE DATABASE {TEMPLATE_DB}"))
-    connection.commit()
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option("script_location", ALEMBIC_DIR)
-    alembic_cfg.set_main_option("sqlalchemy.url", f"{DB_BASE}/{TEMPLATE_DB}")
-    command.upgrade(alembic_cfg, "head")
+    connection.execute(text(f"DROP DATABASE IF EXISTS {TEMPLATE_DB} WITH (FORCE)"))
+    connection.execute(text(f"DROP DATABASE IF EXISTS {DB_NAME} WITH (FORCE)"))
+    connection.execute(text(f"CREATE DATABASE {TEMPLATE_DB}"))
+
+alembic_cfg = Config()
+alembic_cfg.set_main_option("script_location", ALEMBIC_DIR)
+alembic_cfg.set_main_option("sqlalchemy.url", f"{DB_BASE}/{TEMPLATE_DB}")
+command.upgrade(alembic_cfg, "head")
 
 with postgres.connect() as connection:
-    connection.execution_options(isolation_level="AUTOCOMMIT").execute(
-        text(f"CREATE DATABASE {DB_NAME} TEMPLATE {TEMPLATE_DB}")
-    )
-    connection.commit()
+    connection.execute(text(f"CREATE DATABASE {DB_NAME} TEMPLATE {TEMPLATE_DB}"))
 while True:
     sleep(60)
