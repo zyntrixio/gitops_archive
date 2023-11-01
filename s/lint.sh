832e7d900aae44f738b97a63436fcf84737c159d
Chris Pressland <mail@cpressland.io>
2023-11-01 12:30:07 +0000

    more debugging

--- s/lint.sh
+++ s/lint.sh
@@ -2,7 +2,7 @@
 
 command -v rg >/dev/null 2>&1 || { echo >&2 "ripgrep not found. Aborting."; exit 1; }
 
-if rg -Ul '[^\n]\z'; then
+if rg -Ul --debug '[^\n]\z'; then
     echo "Files must end in a newline character" >&2
     exit 1
 else
