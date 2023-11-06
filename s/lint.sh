35383ad03d9d0c2554f84aa0ddd125eb1570820c
Thenuja Viknarajah <thenuja_1@outlook.com>
2023-11-06 10:51:51 +0000

    trailing white spaces remove

--- s/lint.sh
+++ s/lint.sh
@@ -2,10 +2,28 @@
 
 command -v rg >/dev/null 2>&1 || { echo >&2 "ripgrep not found. Aborting."; exit 1; }
 
-if rg -Ul '[^\n]\z' .; then
-    echo "Files must end in a newline character" >&2
-    exit 1
-else
-    echo "All files end in a newline character"
-    exit 0
-fi
+function find_newlines {
+    if rg -Ul '[^\n]\z' .; then
+        echo "Files must end in a newline character" >&2
+        return 1
+    else
+        echo "All files end in a newline character"
+        return 0
+    fi
+}
+
+function find_trailing_whitespace {
+    if rg -Ul '[ \t]+$' .; then
+        echo "Files must not end in trailing whitespace" >&2
+        return 1
+    else
+        echo "All files end in trailing whitespace"
+        return 0
+    fi
+}
+
+echo 'Running lint checks...'
+echo 'Running newline check...'
+find_newlines
+echo 'Running trailing whitespace check...'
+find_trailing_whitespace
